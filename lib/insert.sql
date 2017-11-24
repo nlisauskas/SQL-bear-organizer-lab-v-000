e770b9a95dadf33392a242315544cb2a1e72717a
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-24 21:02:53 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears ("Mr. Chocolate", 22, "M", "Red", "Happy", TRUE);
+INSERT INTO bears ("Rowdy", 22, "M", "Red", "Happy", TRUE);
+INSERT INTO bears ("Tabitha", 22, "M", "Red", "Happy", TRUE);
+INSERT INTO bears ("Sergeant Brown", 22, "M", "Red", "Happy", TRUE);
+INSERT INTO bears ("Melissa", 22, "M", "Red", "Happy", TRUE);
+INSERT INTO bears ("Grinch", 22, "M", "Red", "Happy", TRUE);
+INSERT INTO bears ("Wendy", 22, "M", "Red", "Happy", TRUE);
+INSERT INTO bears (NULL, 22, "M", "Red", "Happy", TRUE);
