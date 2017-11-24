17c30d68b5e250e5492c50c86bc92d2e4ad4b82f
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-24 21:18:49 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Mr. Chocolate", 22, "M", "Red", "Happy", "TRUE");
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Rowdy", 22, "M", "Red", "Happy", "TRUE");
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Tabitha", 22, "M", "Red", "Happy", "TRUE");
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Sergeant Brown", 22, "M", "Red", "Happy", "TRUE");
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Melissa", 22, "M", "Red", "Happy", "TRUE");
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Grinch", 22, "M", "Red", "Happy", "TRUE");
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Wendy", 22, "M", "Red", "Happy", "TRUE");
+INSERT INTO bears (name, age, gender, color, temperament, alive) values (NULL, 22, "M", "Red", "Happy", "TRUE");
